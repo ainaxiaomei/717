3a2daa6817912f2bc4284cfb66bd9418a70681bb
sunqi@snailgame.net <sunqi@snailgame.net>
2017-07-17 18:57:19 +0800

    'ok'

--- spider-ui/src/index.js
+++ spider-ui/src/index.js
@@ -1,5 +1,10 @@
 import './index.html';
 import './index.less';
+import './static/h-ui/css/H-ui.css'
+import './static/h-ui.admin/css/H-ui.admin.css'
+import './lib/Hui-iconfont/1.0.8/iconfont.css'
+import './static/h-ui.admin/skin/default/skin.css'
+import './static/h-ui.admin/css/style.css'
 import dva from 'dva';
 
 // 1. Initialize
