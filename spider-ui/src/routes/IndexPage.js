4c3cddc151b8c270399e3f7ae715d7e14473bb15
sunqi@snailgame.net <sunqi@snailgame.net>
2017-07-18 18:02:17 +0800

    dva

--- spider-ui/src/routes/IndexPage.js
+++ spider-ui/src/routes/IndexPage.js
@@ -1,18 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-import styles from './IndexPage.less';
-
+import MainLayout from "../components/MainLayout";
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <h1>Welcome to dva!</h1>
-      <hr />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/sorrycc/blog/issues/8" target="_blank">Getting Started</a></li>
-      </ul>
-    </div>
+    <MainLayout>
+    </MainLayout>
   );
 }
 
