e0854ecc90564b24a88222736f0dbc030c07ed1c
unknown <ainaxiaomei@gmail.com>
2017-09-03 00:08:44 +0800

    统计

--- web/src/routes/statistic/detail/components/sales.js
+++ web/src/routes/statistic/detail/components/sales.js
@@ -8,7 +8,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 function Sales ({ data }) {
   return (
     <div className={styles.sales}>
-      <div className={styles.title}>Yearly Sales</div>
+      <div className={styles.title}>数据统计</div>
       <ResponsiveContainer minHeight={360}>
         <LineChart data={data}>
           <Legend verticalAlign="top"
@@ -19,7 +19,7 @@ function Sales ({ data }) {
               </ul>)
             }}
           />
-          <XAxis dataKey="name" axisLine={{ stroke: color.borderBase, strokeWidth: 1 }} tickLine={false} />
+          <XAxis dataKey="day" axisLine={{ stroke: color.borderBase, strokeWidth: 1 }} tickLine={false} />
           <YAxis axisLine={false} tickLine={false} />
           <CartesianGrid vertical={false} stroke={color.borderBase} strokeDasharray="3 3" />
           <Tooltip
@@ -29,9 +29,7 @@ function Sales ({ data }) {
               return <div className={styles.tooltip}><p className={styles.tiptitle}>{content.label}</p><ul>{list}</ul></div>
             }}
           />
-          <Line type="monotone" dataKey="Food" stroke={color.purple} strokeWidth={3} dot={{ fill: color.purple }} activeDot={{ r: 5, strokeWidth: 0 }} />
-          <Line type="monotone" dataKey="Clothes" stroke={color.red} strokeWidth={3} dot={{ fill: color.red }} activeDot={{ r: 5, strokeWidth: 0 }} />
-          <Line type="monotone" dataKey="Electronics" stroke={color.green} strokeWidth={3} dot={{ fill: color.green }} activeDot={{ r: 5, strokeWidth: 0 }} />
+          <Line type="monotone" dataKey="data" stroke={color.purple} strokeWidth={3} dot={{ fill: color.purple }} activeDot={{ r: 5, strokeWidth: 0 }} />
         </LineChart>
       </ResponsiveContainer>
     </div>
--- web/src/routes/statistic/detail/index.js
+++ web/src/routes/statistic/detail/index.js
@@ -14,39 +14,59 @@ const bodyStyle = {
 }
 
 function StatisticDetail ({ statisticDetail }) {
-  const { sales, } = {};
-
   const {data} = statisticDetail;
-  const browser = [
-          {
-            "day": "1 号",
-            "data": 11
-          },
-          {
-            "day": "2 号",
-            "data": 12
-          },
-          {
-            "day": "3 号",
-            "data": 13
-          },
-          {
-            "day": "4 号",
-            "data": 113
-          },{
-            "day": "5 号",
-            "data": 113
-          },{
-            "day": "6 号",
-            "data": 113
-          },{
-            "day": "7 号",
-            "data": 113
-          },{
-            "day": "8 号",
-            "data": 113
-          }
-        ]
+  const days = [
+  "d1","d2","d3","d4","d5","d6","d7","d8","d9","d10",
+  "d11","d12","d13","d14","d15","d16","d17","d18","d19","d20",
+  "d21","d22","d23","d24","d25","d26","d27","d28","d29","d30",
+  ];
+  console.log(data[days[2]]);
+  var browser = new Array();
+  for(let i = 0;i<=30;i++){
+    if(!data[days[i]]){
+
+      break;
+
+    }else{
+      var obj = new Object();
+      obj.day = `${i + 1} 号`;
+      obj.data = data[days[i]]
+      browser.push(obj);
+    }
+   
+  }
+  // const browser = [
+  //         {
+  //           "day": "1 号",
+  //           "data": 11
+  //         },
+  //         {
+  //           "day": "2 号",
+  //           "data": 12
+  //         },
+  //         {
+  //           "day": "3 号",
+  //           "data": 13
+  //         },
+  //         {
+  //           "day": "4 号",
+  //           "data": 113
+  //         },{
+  //           "day": "5 号",
+  //           "data": 113
+  //         },{
+  //           "day": "6 号",
+  //           "data": 113
+  //         },{
+  //           "day": "7 号",
+  //           "data": 113
+  //         },{
+  //           "day": "8 号",
+  //           "data": 113
+  //         }
+  //       ]
+
+  const sales = browser; 
   return (
     <Row gutter={24}>
       <Col lg={18} md={24}>
