c5e964d69a8376d5828f3a9badeda0efc6ee75e4
sunqi@snailgame.net <sunqi@snailgame.net>
2017-09-26 18:19:25 +0800

    用户

--- web/src/routes/user/List.js
+++ web/src/routes/user/List.js
@@ -25,25 +25,13 @@ const List = ({ onDeleteItem, onEditItem, isMotion, location, ...tableProps }) =
 
   const columns = [
     {
-      title: 'Avatar',
-      dataIndex: 'avatar',
-      key: 'avatar',
-      width: 64,
-      className: styles.avatar,
-      render: (text) => <img alt={'avatar'} width={24} src={text} />,
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username',
     }, {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text, record) => <Link to={`user/${record.id}`}>{text}</Link>,
-    }, {
-      title: 'NickName',
-      dataIndex: 'nickName',
-      key: 'nickName',
-    }, {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: '密码',
+      dataIndex: 'password',
+      key: 'password',
     }, {
       title: 'Gender',
       dataIndex: 'isMale',
@@ -51,23 +39,11 @@ const List = ({ onDeleteItem, onEditItem, isMotion, location, ...tableProps }) =
       render: (text) => <span>{text
             ? 'Male'
             : 'Female'}</span>,
-    }, {
-      title: 'Phone',
-      dataIndex: 'phone',
-      key: 'phone',
-    }, {
+    },{
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
-    }, {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    }, {
-      title: 'CreateTime',
-      dataIndex: 'createTime',
-      key: 'createTime',
-    }, {
+    },{
       title: 'Operation',
       key: 'operation',
       width: 100,
@@ -82,7 +58,6 @@ const List = ({ onDeleteItem, onEditItem, isMotion, location, ...tableProps }) =
     current: tableProps.pagination.current,
   }
 
-  const getBodyWrapper = body => { return isMotion ? <AnimTableBody {...getBodyWrapperProps} body={body} /> : body }
 
   return (
     <div>
@@ -90,11 +65,11 @@ const List = ({ onDeleteItem, onEditItem, isMotion, location, ...tableProps }) =
         {...tableProps}
         className={classnames({ [styles.table]: true, [styles.motion]: isMotion })}
         bordered
-        scroll={{ x: 1250 }}
+        scroll={{ x: 800}}
         columns={columns}
         simple
         rowKey={record => record.id}
-        getBodyWrapper={getBodyWrapper}
+        pagination = {false}
       />
     </div>
   )
